bbedacb0797ed03b80990ef642ffe06cd4a7b4e1
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-11-30 13:28:46 +0000

    added conditions to show/hide elements & set States

--- app/script.js
+++ app/script.js
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 
 const App = () => {
-  return (
-    <div>
-      <h1>Protect your eyes</h1>
-      <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-      <p>This app will help you track your time and inform you when it's time to rest.</p>
-      <img src="./images/work.png" />
-      <img src="./images/rest.png" />
-      <div className="timer">
-        18:23
-      </div>
-      <button className="btn">Start</button>
-      <button className="btn">Stop</button>
-      <button className="btn btn-close">X</button>
-    </div>
-  )
+	const [status, setStatus] = useState('rest');
+	const [time, setTime] = useState(1200);
+	const [timemr, setTimer] = useState(null);
+
+	const formatTime = time => {
+		const mins = Math.floor((time / 60) % 60)
+			.toString()
+			.padStart(2, '0');
+		const secs = (time % 60).toString().padStart(2, '0');
+
+		return `${mins}:${secs}`;
+	};
+
+	console.log(formatTime(time));
+
+	return (
+		<div>
+			<h1>Protect your eyes</h1>
+			<div className={status === 'off' ? 'show' : 'hide'}>
+				<p>
+					According to optometrists in order to save your eyes, you should
+					follow the 20/20/20. It means you should to rest your eyes every 20
+					minutes for 20 seconds by looking more than 20 feet away.
+				</p>
+				<p>
+					This app will help you track your time and inform you when it's time
+					to rest.
+				</p>
+			</div>
+			<img
+				src="./images/work.png"
+				className={status === 'work' ? 'show' : 'hide'}
+			/>
+			<img
+				src="./images/rest.png"
+				className={status === 'rest' ? 'show' : 'hide'}
+			/>
+			<div className={`timer ${status !== 'off' ? 'show' : 'hide'}`}>
+				{formatTime(time)}
+			</div>
+			<button className={`btn ${status === 'off' ? 'show' : 'hide'}`}>
+				Start
+			</button>
+			<button className={`btn ${status !== 'off' ? 'show' : 'hide'}`}>
+				Stop
+			</button>
+			<button className="btn btn-close">X</button>
+		</div>
+	);
 };
 
 render(<App />, document.querySelector('#app'));
